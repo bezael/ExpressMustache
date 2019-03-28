8e225e31e3bc48ae2a6f9ec1ac6339295c4d4922
Bezael <bezael.perez.amaris@axa.com>
2019-03-28 10:30:28 +0100

    Finish

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-
+const PORT = 4001;
 const app = express();
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-
 app.get('/', (req, res) => {
-    //res.send('Hello!!');
-    // Whereby the first parameter ‘hello’ refers to the hello.html file 
-    // (no need to include the extension (e.g. hello.html) 
-    // as it has been previously set as html.
     res.render('index', {"title":"MustacheExpress"});
 });
 
@@ -19,4 +14,4 @@ app.get('/user', (req,res) =>{
     res.render('user', {"name": "Dominicode"});    
 });
 
-app.listen(4001, ()=> console.log(`Server running on port 4001`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
