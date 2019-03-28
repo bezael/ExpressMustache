c63bf2ad9b62725d623f1c753212fc631941d6e7
Bezael <bezael.perez.amaris@axa.com>
2019-03-28 08:44:14 +0100

    Add mustacheExpress

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
+
 const app = express();
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + 'src/views');
+
 
 app.get('/', (req, res) => {
     res.send('Hello!!');
